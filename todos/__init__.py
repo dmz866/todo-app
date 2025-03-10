3675039db85b96fb2ba5f3dde65a3dc3255949a7
David Murillo <dmz866@hotmail.com>
2025-03-10 12:08:51 -0500

    flash sql alchemy orm added

--- todos/__init__.py
+++ todos/__init__.py
@@ -1,18 +1,31 @@
 from flask import Flask, render_template
 
 from todos import todo, auth
+from todos.db import get_db
+from . import models
 
 
 def create_app():
     app = Flask(__name__)
 
-    app.config.from_mapping(DEBUG=True, SECRET_KEY='MY_SECRET_KEY')
+    # Configuración del poyecto
+    app.config.from_mapping(
+        DEBUG=False,
+        SECRET_KEY='devtod',
+        SQLALCHEMY_DATABASE_URI="sqlite:///todolist.db"
+    )
 
     app.register_blueprint(todo.bp)
     app.register_blueprint(auth.bp)
 
+    database = get_db()
+    database.init_app(app)
+
     @app.route('/')
     def index():
         return render_template('index.html')
+
+    with app.app_context():
+        database.create_all()
 
     return app
---
+++ todos/db.py
@@ -0,0 +1,13 @@
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+def get_db():
+    return db
+
+
+db = SQLAlchemy(model_class=Base)
